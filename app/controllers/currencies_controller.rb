8945112c1d24816f7f860c7d15b7280e444f275b
tejavanjari <tejavanjari007@gmail.com>
2021-10-29 18:40:32 +0000

    /forex done

---
+++ app/controllers/currencies_controller.rb
@@ -0,0 +1,11 @@
+class CurrenciesController < ApplicationController
+  def first_currency
+    url = 'https://api.exchangerate.host/symbols'
+    # get JSON
+    json = open(url).read
+    # parse JSON to dictionary
+    parsed_json = JSON.parse(json)
+    @array_of_symbols = parsed_json.fetch('symbols').keys
+    render({:template => "currency_templates/step_one.html.erb"})
+  end
+end
